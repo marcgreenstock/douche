fb104ad4a2f77f9ef248609ea69b7e6aee1588a4
Marc Greenstock <marc.greenstock@holidayextras.com>
2011-04-26 10:03:32 +0100

    support for default xmlns

--- test/test.js
+++ test/test.js
@@ -1,7 +1,11 @@
 var douche = require('../index.js');
 
 var client = new douche('http://api.search.live.net/search.wsdl');
-client.wsdl.parse();
+client.wsdl.parse(function(wsdl) {
+  console.log(wsdl);
+});
 
 var client = new douche('http://soap.amazon.com/schemas3/AmazonWebServices.wsdl');
-client.wsdl.parse();
+client.wsdl.parse(function(wsdl) {
+  console.log(wsdl);
+});
